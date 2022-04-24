55ace46788109e3db159a4e1f1454a1f0b29e3dc
p.gabrieldeveloper <paulo.gabriel@atoscapital.com.br>
2022-04-24 12:07:45 -0300

    initial commit paymentServicie created

---
+++ paymentService/src/test/kotlin/com/pgabriel/paymentservice/PaymentServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pgabriel.paymentservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
